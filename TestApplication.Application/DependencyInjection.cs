c2abde98be9b794135b6db0eddb3c5a0d5425030
Mahesh Venkatraman <mahesh.venkatraman@cesltd.com>
2023-10-05 17:34:51 +0530

    Dependencies added as separate class file

---
+++ TestApplication.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TestApplication.Application.Services.Authentication;
+using TestApplication.Application.Services.Users;
+
+namespace TestApplication.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
