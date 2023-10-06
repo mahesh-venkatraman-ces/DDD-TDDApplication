592ec8038ea5f71717df55acb3a37485130934a7
Mahesh Venkatraman <mahesh.venkatraman@cesltd.com>
2023-10-06 11:03:51 +0530

    Test cases for Authentication Service

--- TestApplication.UnitTests/Systems/Services/TestAuthenticationService.cs
+++ TestApplication.UnitTests/Systems/Services/TestAuthenticationService.cs
@@ -1,6 +1,54 @@
-﻿namespace TestApplication.UnitTests.Systems.Services
+﻿using FluentAssertions;
+using Microsoft.Extensions.Options;
+using TestApplication.Application.Services.Authentication;
+using TestApplication.Application.Services.Users;
+using TestApplication.Contracts.Config;
+using TestApplication.Contracts.UserDetails;
+using TestApplication.UnitTests.Fixtures;
+using TestApplication.UnitTests.Helpers;
+
+namespace TestApplication.UnitTests.Systems.Services
 {
-    internal class TestAuthenticationService
+    public class TestAuthenticationService
     {
+        [Fact]
+        public async Task Get_RegisteredWithTestData_ThenOk()
+        {
+            //Arrange
+
+            var expectedResponse = AuthenticationFixtures.GetAuthenticationResult();
+            var request = AuthenticationFixtures.GetRegisterResult();
+
+            var sut = new AuthenticationService();
+
+            //Act
+
+            var result =
+                sut.Register(request.FirstName,request.LastName,request.Email,request.Password);
+
+            //Assert
+
+            Assert.IsType<AuthenticationResult>(result);
+        }
+
+        [Fact]
+        public async Task Get_LoginWithTestData_ThenOk()
+        {
+            //Arrange
+
+            var expectedResponse = AuthenticationFixtures.GetAuthenticationResult();
+            var request = AuthenticationFixtures.GetLoginResult();
+
+            var sut = new AuthenticationService();
+
+            //Act
+
+            var result =
+                sut.Login(request.Email, request.Password);
+
+            //Assert
+
+            Assert.IsType<AuthenticationResult>(result);
+        }
     }
 }
